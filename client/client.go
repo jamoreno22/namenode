ddfdc99912b9e8300ac62788b809ef3a360e29a9
barbaraUribeCataldo <barbara.uribe@sansano.usm.cl>
2020-11-29 01:57:54 -0300

    SendProposal function added

--- client/client.go
+++ client/client.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
-	"reflect"
 
 	gral "github.com/jamoreno22/namenode/pkg/proto"
 	"google.golang.org/grpc"
@@ -23,21 +23,19 @@ func main() {
 
 	//Tiene que ser un stream pero no se cómo hacerlo :v
 	//prop := gral.Proposal{Ip: "8000", Chunk: &gral.Chunk{Name: "Chunk1", Data: []byte("ABC€")}}
-	book := gral.Book{Name: "Libro1", Parts: 3}
+	//book := gral.Book{Name: "Libro1", Parts: 3}
 	proposals := []gral.Proposal{}
 	proposals = append(proposals, gral.Proposal{Ip: "8000", Chunk: &gral.Chunk{Name: "Chunk1", Data: []byte("ABC€")}})
 	proposals = append(proposals, gral.Proposal{Ip: "8001", Chunk: &gral.Chunk{Name: "Chunk2", Data: []byte("ABC2")}})
 
 	runWriteLog(client, proposals)
+	runSendProposal(client, proposals)
 
-	log.Println(reflect.TypeOf(client))
-	client = gral.NewNameNodeClient(conn)
-
-	resp, err := client.GetBookInfo(context.Background(), &book)
-	if err != nil {
-		log.Fatalf("Did not connect: %s", err)
-	}
-	log.Println(resp)
+	//resp, err := client.GetBookInfo(context.Background(), &book)
+	//if err != nil {
+	//	log.Fatalf("Did not connect: %s", err)
+	//}
+	//log.Println(resp)
 
 }
 
@@ -60,4 +58,32 @@ func runWriteLog(nc gral.NameNodeClient, proposals []gral.Proposal) error {
 	}
 	log.Printf("Route summary: %v", reply)
 	return nil
+}
+
+func runSendProposal(nc gral.NameNodeClient, proposals []gral.Proposal) error {
+
+	stream, err := nc.SendProposal(context.Background())
+	if err != nil {
+		log.Println("Error de stream send proposal")
+	}
+	a := 1
+	for _, prop := range proposals {
+		if err := stream.Send(&prop); err != nil {
+			log.Println("error al enviar chunk")
+			log.Fatalf("%v.Send(%d) = %v", stream, a, err)
+		}
+		a = a + 1
+	}
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			// read done.
+			//DistributeChunks()
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Failed to receive a proposal : %v", err)
+		}
+		log.Printf("Got a proposal ip :%s ", in.Ip)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"time"
 
 	gral "github.com/jamoreno22/namenode/pkg/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
-type nameServer struct {
+type nameNodeServer struct {
 	gral.UnimplementedNameNodeServer
 }
 
 var infoBook = gral.Book{}
+
+var receivedProposal []gral.Proposal
 
 func main() {
 
@@ -41,7 +41,7 @@ func main() {
 // - - - - - - - - - - NameNode Server functions - - - - - - - - - - -
 
 // Writelog server side
-func (s *nameServer) WriteLog(wls gral.NameNode_WriteLogServer) error {
+func (s *nameNodeServer) WriteLog(wls gral.NameNode_WriteLogServer) error {
 	log.Printf("Stream WriteLogServer")
 	// create log
 	f, err := os.Create("data.txt")
@@ -66,7 +66,7 @@ func (s *nameServer) WriteLog(wls gral.NameNode_WriteLogServer) error {
 
 		sP = append(sP, *prop)
 
-		// Aquí va el código para guardar el log
+		// Aquí va el código para guardar el log (falta formato)
 		_, err2 := f.WriteString("ip " + prop.Ip)
 
 		if err2 != nil {
@@ -75,8 +75,70 @@ func (s *nameServer) WriteLog(wls gral.NameNode_WriteLogServer) error {
 	}
 }
 
-func (s *nameServer) GetBookInfo(ctx context.Context, book *gral.Book) (*gral.Message, error) {
+/*func (s *nameServer) GetBookInfo(ctx context.Context, book *gral.Book) (*gral.Message, error) {
 
 	infoBook = *book
 	return nil, status.Errorf(codes.Unimplemented, "method GetBookInfo not implemented")
+}*/
+
+//Si quieres algo bien hecho tienes que hacerlo tu mismo
+func generateproposal(props []gral.Proposal) ([]gral.Proposal, error) {
+	ips := []string{"10.10.28.17:9000", "10.10.28.18:9000", "10.10.28.19:9000"}
+	var propResponse []gral.Proposal
+	var gIps []string //ipes wenas
+	for _, ip := range ips {
+		if pingDataNode(ip) {
+			gIps = append(gIps, ip)
+		}
+	}
+	if len(gIps) == 0 {
+		return nil, fmt.Errorf("There's no posible proposal")
+	}
+
+	for _, prop := range props {
+		if !stringInSlice(prop.Ip, gIps) {
+			propResponse = append(propResponse, gral.Proposal{Ip: gIps[0], Chunk: prop.Chunk})
+		}
+	}
+	return propResponse, nil
+}
+
+func pingDataNode(ip string) bool {
+	timeOut := time.Duration(10 * time.Second)
+	_, err := net.DialTimeout("tcp", ip, timeOut)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *nameNodeServer) SendProposal(sps gral.NameNode_SendProposalServer) error {
+	for {
+		prop, err := sps.Recv()
+		if err == io.EOF {
+			props, err2 := generateproposal(receivedProposal)
+			if err2 != nil {
+				log.Printf("Oh no!: %v", err2)
+			}
+			//s.WriteLog()
+			for _, p := range props {
+				if err3 := sps.Send(&p); err3 != nil {
+					return err3
+				}
+			}
+		}
+		if err != nil {
+			return err
+		}
+		receivedProposal = append(receivedProposal, *prop)
+	}
 }
